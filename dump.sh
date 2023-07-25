a493cf2fa12ce47176986eaa60d53c09aad54e43
Tommy Liu <tommyliu722@hotmail.com>
2023-07-25 11:13:25 +0800

    [feat]修改資料庫匯出用script匯出檔案的位置

--- dump.sh
+++ dump.sh
@@ -6,4 +6,4 @@ NOW=$(date +'%Y-%m-%d-%H:%M:%S')
 
 # export wordPress database
 mv db/default/wp.sql db/backup/wp-"$NOW".sql # backup
-docker exec -it ${COMPOSE_PROJECT_NAME}_mysql-maria sh -c "MYSQL_PWD=$WP_MYSQL_ROOT_PASSWORD mysqldump -uroot $WP_MYSQL_DATABASE" > db/default/wp.sql # export to db/default/wp.sql
+docker exec -it ${COMPOSE_PROJECT_NAME}_mysql-maria sh -c "MYSQL_PWD=$WP_MYSQL_ROOT_PASSWORD mysqldump -uroot $WP_MYSQL_DATABASE" > db/wp.sql # export to db/wp.sql
