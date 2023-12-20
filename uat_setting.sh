80e70e9912f515a2e2a72f2737b785f97c6e9bd9
Tommy Liu <tommyliu722@hotmail.com>
2023-12-20 16:15:39 +0800

    [feat]新增設定測試環境用script

---
+++ uat_setting.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# 引入.env檔參數
+if [ -f .env ]; then
+    export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+fi
+
+# 檢查是否已定義必要的變數
+if [ -z "$COMPOSE_PROJECT_NAME" ] || [ -z "$WP_URL" ]; then
+    echo "未定義必要的變數: COMPOSE_PROJECT_NAME, WP_URL。請檢查 .env 檔案。"
+    exit 1
+fi
+
+UAT_DOMAIN=$(echo "$WP_URL" | sed 's/.*\/\///; s/\/.*//')
+if [ -z "$UAT_DOMAIN" ]; then
+    echo "無法取得Domain設定。請檢查 .env 檔案。"
+    exit 1
+fi
+
+echo "UAT_DOMAIN = $UAT_DOMAIN"
+
+# 檢查Docker是否已安裝
+if [ -x /usr/bin/docker ]; then
+    echo "Docker已安裝!!"
+else
+    echo "********** Docker尚未安裝，開始安裝... **********"
+    # 執行安裝腳本
+    sudo sh deployment_scripts/docker_install.sh
+fi
+
+# 檢查Nginx是否已安裝
+if [ -x /usr/sbin/nginx ]; then
+    echo "Nginx已安裝!!"
+else
+    echo "********** Nginx尚未安裝，開始安裝... **********"
+    # 執行安裝腳本
+    sudo sh deployment_scripts/nginx_install.sh
+fi
+
+# 建立nginx設定檔
+echo "********** 建立nginx設定檔 **********"
+sudo sh deployment_scripts/nginx_setting.sh $COMPOSE_PROJECT_NAME $UAT_DOMAIN
+
+# 修改wp.sql中的連結
+echo "********** 修改wp.sql中的連結設定 **********"
+sudo sh deployment_scripts/repleace_db_url.sh db/uat/wp.sql $WP_URL
+
+# 啟動 docker
+echo "********** 啟動 docker **********"
+sudo docker-compose up -d
+
+# 修改wp-content/uploads的資料夾權限
+echo "********** 修改wp-content/uploads的資料夾權限 **********"
+sudo sh deployment_scripts/wp_permission_setting.sh $COMPOSE_PROJECT_NAME
+
+echo "設定完成！！"
